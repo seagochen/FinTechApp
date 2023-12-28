ec7f7d3c52566c2f8b39058cbdb3684e7e497637
Orlando Chen <seagochen@hotmail.com>
2023-12-29 03:23:14 +0900

    Angular追加された

---
+++ 3_Frontend/AngularApp/src/app/accountant/accountant.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-accountant',
+  standalone: true,
+  imports: [],
+  templateUrl: './accountant.component.html',
+  styleUrl: './accountant.component.css'
+})
+export class AccountantComponent {
+
+}
