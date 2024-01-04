9dfe120860608c0ecfbee180f903611fe497b9e6
Orlando Chen <seagochen@hotmail.com>
2024-01-04 20:12:13 +0900

    ソースコードを修正された

---
+++ 3_Frontend/AngularApp/src/app/account-keeper/account-keeper.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-account-keeper',
+  standalone: true,
+  imports: [],
+  templateUrl: './account-keeper.component.html',
+  styleUrl: './account-keeper.component.css'
+})
+export class AccountKeeperComponent {
+
+}
