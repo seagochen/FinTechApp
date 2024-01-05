c16f0b9dfda0a3018ab0e4b91d9f195cce3b99c7
Orlando Chen <xiantong.chen.ext@seimens-healthineers.com>
2024-01-05 17:14:14 +0900

    py flask endside app. 基本的なFrameworkは完成された。

---
+++ 2_Backend/LedgerServer/app/__init__.py
@@ -0,0 +1,23 @@
+from flask import Flask
+# from flask_sqlalchemy import SQLAlchemy
+
+# Register the views
+from app import Views
+
+
+# db = SQLAlchemy()
+
+def create_app(config_filename):
+    app = Flask(__name__)
+    # app.config.from_object(config_filename)
+
+    # db.init_app(app)
+
+    # Register blueprint
+    from app.Views import bp
+    app.register_blueprint(bp)
+
+    # with app.app_context():
+    #     db.create_all()
+
+    return app
