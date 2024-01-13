2869e80abdd206268a8d68abbe596cd71ff1dadb
Orlando Chen <seagochen@hotmail.com>
2024-01-14 00:13:57 +0900

    プログラムロード後に表示されるインデックスのエラーを修正しました。

--- 3_Frontend/react/src/components/Home.js
+++ 3_Frontend/react/src/components/Home.js
@@ -1,7 +1,26 @@
 import React from "react";
 
 function Home() {
-    return <h1>Home</h1>
+    return (
+        <div class="bg-home">
+
+             {/*White text floating in the middle of the screen */}
+            <div class="bg-text"
+                 style="color: white; font-size: 40px; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); z-index: 2;">
+                <h1>Hey, here you are!</h1>
+                <p>NECESSITAS EST INGENII MATER!</p>
+
+                {/* This is the button, in the middle of the screen */}
+                <div class="containser">
+                    <div class="d-grid gap-2 d-md-flex justify-content-md-center">
+
+                        {/* This will show a button here */}
+                        <a href="./catalog.html" class="btn btn-primary me-md-2" type="button">Learn More</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Home;
--- 3_Frontend/react/src/index.js
+++ 3_Frontend/react/src/index.js
@@ -1,13 +1,60 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter, Route } from "react-router-dom"
-import App from './App'
+
+import Home from "./components/Home";
+import About from "./components/About";
+import Shop from "./components/Shop";
+import Error from "./components/Error";
+import Navbar from "./components/Navbar";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+
+function Navibar() {
+    return (
+        // <main>
+        //     <Navbar />
+        //     <Routes>
+        //         <Route path="/" component={Home} exact />
+        //         <Route path="/about" component={About} />
+        //         <Route path="/shop" component={Shop} />
+        //         <Route component={Error} />
+        //     </Routes>
+        // </main>
+
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="/">Orlando's Blog</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="nav nav-pills">
+                        <li className="nav-item">
+                            <a className="nav-link" routerLink="/home" routerLinkActive="active"
+                               ariaCurrentWhenActive="page">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" routerLink="/account-keeper" routerLinkActive="active"
+                               ariaCurrentWhenActive="page">Account Keeper</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" routerLink="/ledger-keeper" routerLinkActive="active"
+                               ariaCurrentWhenActive="page">Ledger Keeper</a>
+                        </li>
+                    </ul>
+                </div>
+
+            </div>
+        </nav>
+    )
+}
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
     <BrowserRouter>
-        <App />
+        <Navibar/>
     </BrowserRouter>
 )
